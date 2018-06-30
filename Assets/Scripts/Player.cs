6fc12a98c057ca0bee15e19c807b7e40d5e1ed08
Erno Lokkila <eolokk@utu.fi>
2018-06-30 18:15:21 +0300

    Player can now be created

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,21 +5,7 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class Player : MonoBehaviour {
     
-    [SerializeField]
-    private int multiplier = 5;
-    
     int foodCount = 0;
-
-    void Update() {
-        Vector2 direction = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        float distance = -Vector2.Distance(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition));
-        if (Input.GetMouseButtonDown(0)) {
-            Destroy(this);
-            GetComponent<Rigidbody2D>().AddForce(multiplier * distance * direction);
-        }
-        //rigidbody.AddForce(Input.mousePosition);
-
-    }
 
     void OnCollisionEnter2D(Collision2D col)
     {

30ea62509fc2a36937312b776c2f980e73945e87
Erno Lokkila <eolokk@utu.fi>
2018-06-30 17:31:01 +0300

    Aim bar done

---
+++ Assets/Scripts/AimBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimBar : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        transform.localScale = new Vector3(Mathf.Min(5+Vector2.Distance(Camera.main.ScreenToWorldPoint(Input.mousePosition), transform.position),10),1,22);
+
+        if(Input.GetMouseButtonDown(0))
+            Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,14 +8,12 @@ public class Player : MonoBehaviour {
     [SerializeField]
     private int multiplier = 5;
 
-	// Update is called once per frame
-	void Update () {
+    void Update() {
         Vector2 direction = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
+        float distance = -Vector2.Distance(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition));
         if (Input.GetMouseButtonDown(0)) {
-            float distance = -Vector2.Distance(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            Destroy(this);
             GetComponent<Rigidbody2D>().AddForce(multiplier * distance * direction);
         }
-        //rigidbody.AddForce(Input.mousePosition);
     }
 }

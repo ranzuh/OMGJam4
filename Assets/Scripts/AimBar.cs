41481d400ca36babbf452102e078e3a54f82873d
Erno Lokkila <eolokk@utu.fi>
2018-06-30 19:21:14 +0300

    Aim line is now red

--- Assets/Scripts/AimBar.cs
+++ Assets/Scripts/AimBar.cs
@@ -10,7 +10,7 @@ public class AimBar : MonoBehaviour {
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
-        transform.localScale = new Vector3(Mathf.Min(5+Vector2.Distance(Camera.main.ScreenToWorldPoint(Input.mousePosition), transform.position),10),1,22);
+        transform.localScale = new Vector3(Mathf.Min(1+Vector2.Distance(Camera.main.ScreenToWorldPoint(Input.mousePosition), transform.position),10),1,22);
 
         if(Input.GetMouseButtonDown(0))
             Destroy(this.gameObject);

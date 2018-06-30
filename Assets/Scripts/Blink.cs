405b56e1bba78a2984d168b7edb75d4efe71a43b
Erno Lokkila <eolokk@utu.fi>
2018-06-30 21:57:54 +0300

    Start area

---
+++ Assets/Scripts/Blink.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour {
+
+    public float curTime = 0;
+	// Update is called once per frame
+	void Update () {
+        Color c = GetComponent<SpriteRenderer>().material.GetColor("_Color");
+        
+        curTime += Time.deltaTime;
+        Debug.Log(Mathf.Lerp(0, 1, curTime / 1000));
+        Color newColor = new Color(c.r, c.g, c.b, Mathf.Lerp(0.5f, 0.75f, Mathf.Sin(curTime*Mathf.PI)));
+        GetComponent<SpriteRenderer>().material.SetColor("_Color", newColor);
+
+        transform.Rotate(new Vector3(0,0,0.5f));
+	}
+}
---
+++ Assets/Scripts/StartArea.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartArea : MonoBehaviour {
+
+    [SerializeField]
+    GameObject playerPrefab;
+
+	// Use this for initialization
+	void OnMouseUp () {
+        GameObject obj = Instantiate(playerPrefab);
+        Vector3 v = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        obj.transform.position = new Vector3(v.x, v.y, 0);
+
+        Destroy(this);
+	}
+}

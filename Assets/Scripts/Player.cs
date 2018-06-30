e708daaa42070ec6ec99ec76aa126f0cd0b5d0d4
Erno Lokkila <eolokk@utu.fi>
2018-06-30 13:47:21 +0300

    player prefab

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Player : MonoBehaviour {
+
+    Rigidbody2D rigidbody;
+    [SerializeField]
+    Vector2 momentum;
+
+    // Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        momentum = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("dsaf");
+            rigidbody.AddForce(100*momentum);
+        }
+        //rigidbody.AddForce(Input.mousePosition);
+    }
+}

cfe1b4fe05f80b750ccfbeb536b096836c6b5d2c
Erno Lokkila <eolokk@utu.fi>
2018-07-02 01:10:30 +0300

    Added force fields to everything

---
+++ Assets/Scripts/ForceField.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceField : MonoBehaviour {
+
+    public float fieldScale = 5;
+    public float time;
+    public bool repulse = false;
+
+    private void Awake()
+    {
+        enabled = false;
+    }
+
+    void Start()
+    {
+        transform.localScale = Vector3.zero;
+        StartCoroutine("MoveField");
+    }
+
+    IEnumerator MoveField () {
+        float duration = 5;
+        time = 0;
+        Vector3 start;
+        Vector3 finish;
+        if (repulse)
+        {            
+            finish = new Vector3(fieldScale, fieldScale, fieldScale);
+            start = Vector3.zero;
+        }
+        else {
+            start = new Vector3(fieldScale, fieldScale, fieldScale);
+            finish = Vector3.zero;
+        }
+        while (true) {
+            transform.localScale = Vector3.Lerp(start, finish, time/duration);
+            time += 0.01f;
+            if (Vector3.Distance(transform.localScale, finish) <0.1f)
+            {
+                transform.localScale = start;
+                time = 0;
+            }
+            yield return new WaitForSeconds(0.01f);
+        }
+	}
+}
---
+++ Assets/Scripts/ForceFieldGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceFieldGenerator : MonoBehaviour {
+
+    [SerializeField]
+    GameObject field;
+    [SerializeField]
+    private bool repulse = false;
+
+    public int divider = 1;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine("GenerateFields");
+	}
+	
+	// Update is called once per frame
+	IEnumerator GenerateFields () {
+        for (int i = 0; i < 4; i++) {
+            GameObject go = Instantiate(field, transform);
+            go.GetComponent<ForceField>().fieldScale = transform.GetChild(0).localScale.x/divider;
+            go.GetComponent<ForceField>().transform.localPosition = Vector3.zero;
+            go.GetComponent<ForceField>().repulse = repulse;
+            go.GetComponent<ForceField>().enabled = true;            
+            yield return new WaitForSeconds(2f);
+        }
+	}
+}

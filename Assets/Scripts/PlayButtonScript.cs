4f192566bbb91b396f6f92a78ed4207d57f41cc5
Erno Lokkila <eolokk@utu.fi>
2018-07-01 22:05:02 +0300

    Bonus level to main menu

--- Assets/Scripts/PlayButtonScript.cs
+++ Assets/Scripts/PlayButtonScript.cs
@@ -1,9 +1,32 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
 
 public class PlayButtonScript : MonoBehaviour{
 
+    public GameObject bonusButton;
+
 	public void PlayScene() {
         SceneManager.LoadScene(1);
+    }
+
+    public void PlayBonusLevel() {
+        SceneManager.LoadScene("BonusLevel");
+    }
+
+    void Update()
+    {
+        if (Input.inputString.ToLower().Equals("b")) {
+            StartCoroutine("MakeMagicButtonAppear");
+        }
+    }
+
+    IEnumerator MakeMagicButtonAppear() {
+        yield return new WaitForSeconds(5);
+        if (Input.inputString.ToLower().Equals("b"))
+        {
+            bonusButton.SetActive(true);
+        }
     }
 }

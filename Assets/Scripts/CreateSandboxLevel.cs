c849ff806354a44d536be31c9f32c11f452ecd82
Erno Lokkila <eolokk@utu.fi>
2018-07-01 21:44:17 +0300

    BonusLevel

---
+++ Assets/Scripts/CreateSandboxLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateSandboxLevel : MonoBehaviour {
+
+    [SerializeField]
+    int foods = 50;
+    [SerializeField]
+    int radius = 40;
+    [SerializeField]
+    GameObject food;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < foods; i++) {
+            Vector2 position = UnityEngine.Random.insideUnitCircle;
+            Instantiate(food, new Vector3(position.x, position.y, 0)*radius, Quaternion.identity);
+        }
+        Destroy(gameObject);
+    }
+
+}
